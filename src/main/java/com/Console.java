ac349d2524e5e3d6e88dee5f9caff1f6a6323de2
wangyong <yundan_wy@126.com>
2016-10-19 18:33:04 +0800

    项目初始化

---
+++ src/main/java/com/Console.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序入口
+ * Created by wangyong on 16-10-19.
+ */
+@SpringBootApplication
+public class Console {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Console.class, args);
+    }
+}
