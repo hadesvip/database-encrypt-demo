179ce8ee61158681064d67c6d0272f0d830bb884
wangyong <yundan_wy@126.com>
2016-10-20 22:28:24 +0800

    '添加配置'

--- src/main/java/com/Console.java
+++ src/main/java/com/Console.java
@@ -2,12 +2,14 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * 程序入口
  * Created by wangyong on 16-10-19.
  */
 @SpringBootApplication
+//@EnableConfigurationProperties
 public class Console {
 
     public static void main(String[] args) {
--- src/main/java/com/componet/DecryptDruidSource.java
+++ src/main/java/com/componet/DecryptDruidSource.java
@@ -4,22 +4,21 @@ import com.alibaba.druid.filter.config.ConfigTools;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 对用户名进行加密
  * Created by wangyong on 16-10-20.
  */
+@Configuration
 @ConfigurationProperties(locations = "classpath:application.yml")
 public class DecryptDruidSource extends DruidDataSource {
 
     @Value("${mysql.jdbc.username_publickey}")
     private String userNameKey;
 
-
     @Override
     public void setUsername(String username) {
-
-        userNameKey="MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJO42vzkepf+kvNh2s+4S9SnHcYo/BuECnzrSkzwqkHVguGlcRCrh38qUvV4a1AvXI9eC+OZT63HfW0hOiWKw68CAwEAAQ==";
         try {
             username = ConfigTools.decrypt(userNameKey, username);
         } catch (Exception e) {
